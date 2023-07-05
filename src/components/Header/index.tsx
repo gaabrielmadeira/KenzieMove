7c64f84145bfc4e6505705fd70359d1fcb33bff2
Gabriel Madeira <gabscastello95@gmailcom>
2023-07-05 10:28:57 -0300

    feat-globalstyles-arquive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,14 @@
 import kenziemovie from "../../assets/kenziemovie.svg";
-import { StyledHeader } from "./style";
 
 export const Header = () => (
-  <StyledHeader>
+  <header>
     <img src={kenziemovie} alt="imagem logo" width="50px" height="50px" />
 
     <div>
       <button>Cadastre-se</button>
       <button>Entrar</button>
     </div>
-  </StyledHeader>
+  </header>
 );
 
 
